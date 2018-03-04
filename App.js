98655c9cd0dd84526a68b0364ba7e11b0a2399ae
Naga durga Prasad E <durgaprasad814@gmail.com>
2018-03-05 01:51:04 +0530

    added task to calendar

--- App.js
+++ App.js
@@ -15,8 +15,6 @@ import {
   LayoutAnimation,
   UIManager
 } from 'react-native';
-
-import Cell from "./src/grid/row/cell";
 import Row from "./src/grid/row";
 
 var moment = require('moment');
@@ -51,11 +49,8 @@ var weekHourArr = new Array();
 for(i=0; i < 168; i++ ){
   let h = moment.duration(i, 'h');
   let timeObj = firstOfWeek.clone().add(h)
-  if(timeObj.isBetween('2018-03-05', '2010-03-06')){
-  weekHourArr.push({timeObj, MonthDay:timeObj.date(), weekDay: timeObj.day(), hour: timeObj.hours()});        
-  } else{
-  weekHourArr.push({timeObj, MonthDay:timeObj.date(), weekDay: timeObj.day(), hour: timeObj.hours()});    
-  }
+  weekHourArr.push({timeObj, MonthDay:timeObj.date(), weekDay: timeObj.day(), hour: timeObj.hours(), task: [] });
+
 }
 
 // Create bodyArr which renders row
@@ -69,6 +64,129 @@ for(i= 0; i < 24; i++){
  let headTitle = firstOfWeek.clone().add(h).format("hh a")
  bodyArr.push({id:''+i, headTitle,  data:tempArr});
 }
+
+// Helper funtions
+
+// 1) firstDayOfWeek get first day of week, that contains given date.
+// needs date and start of the week parameter.
+// return a moment obeject
+
+
+
+const firstDayOfWeek = function(date, weekStart) {
+  return moment(date).startOf('week').weekday(weekStart);
+}
+
+// 2) headerArrCreator returns an array of 7 objects. Containing 
+// each object contains
+// dayName = "Sun"/ "Fri"
+// weekDay = 0 / 5
+// dayNum = 4 ,if date is 4th May 
+// monthName = May // May the force be with you 
+// timeObj a moment time object, so that you can use it for further transfroms
+// needs date and start of the week parameter.
+
+
+const headerArrCreator = function(date, weekStart){
+  let firstDay = firstDayOfWeek(date, weekStart);
+  let headerArr = new Array();
+
+    for(i=0; i < 7; i++ ){
+      let d = moment.duration(i, 'd');
+      let timeObj = firstDay.clone().add(d)
+      headerArr.push({timeObj, weekDay: timeObj.day(), dayNum:timeObj.date(),dayName: timeObj.format("ddd"), monthName: timeObj.format("MMM") });
+    }
+  return headerArr;
+}
+
+// 3) bodyArrCreator returns an array of 168 objects. Containing 
+// each object contains
+// weekDay = 0 / 5
+// MonthDay = 4 ,if date is 4th May 
+// hour = 0 to 23 // this is to track which row should render this object
+// timeObj a moment time object, so that you can use it for further transfroms
+// task a object of sub task
+// needs date and start of the week parameter.
+
+
+const bodyArrCreator = function(date, weekStart) {
+  let firstDay = firstDayOfWeek(date, weekStart);
+  let bodyArr = new Array();
+
+  for(i=0; i < 168; i++ ){
+    let h = moment.duration(i, 'h');
+    let timeObj = firstDay.clone().add(h)
+    bodyArr.push({timeObj, MonthDay:timeObj.date(), weekDay: timeObj.day(), hour: timeObj.hours(), task:[] });
+  }
+
+return bodyArr;
+}
+
+// 4) rotationFilter returns an array of filter objects. 
+// based on start of week parameter, 
+// the assumption is to remove the last 2 columns
+// needs date, isVertical, and start of the week parameter.
+// rotationFilter(bodyArrCreator(new Date(), 0), 0, true)
+
+ const rotationFilter = function( arr, weekStart, isVertical){
+    return isVertical ?
+     arr.slice().filter(result => result.weekDay !== weekStart+5 && result.weekDay !== weekStart+6)
+     :
+     arr
+ }
+
+ 
+ // 5) taskAdder 
+ // Takes taskObj and adds it to calendar array based on task start and end date
+ // returns calendar array
+
+  const taskAdder = function(arr, taskArr){
+    // taskObj should have
+    // 1 start time
+    // 2 end time
+    // 3 task name
+
+
+    let tempArr = arr.slice();
+
+    taskArr.forEach(function(taskObj){
+      const {endTime, startTime, taskName } = taskObj;
+      console.log(taskObj);
+      if(moment.isMoment(endTime) && moment.isMoment(startTime)) {
+        tempArr.forEach(function(element) {
+          let {timeObj, task} = element
+          moment(timeObj).isBetween(startTime.toISOString(), endTime.toISOString(), "hour") ? task.push(taskObj): null
+        });
+      }
+    })
+    return tempArr;
+  }
+
+// 6) calendarArrayCreator 
+// array of task objects , date, weekStart
+
+
+  const calendarArrayCreator = function (taskArr, date, weekStart, isVertical) {
+
+    let calendarArr = new Array();
+    let firstOfWeek = firstDayOfWeek(date, weekStart);
+    let bodyArr = bodyArrCreator(date, weekStart);
+    let taskedArr = taskAdder(bodyArr, taskArr);
+    let rotationfilterArr = rotationFilter(taskedArr, weekStart, isVertical);
+
+    // loop 0 to 23 to get hour rows
+    for(i= 0; i < 24; i++){
+    let tempArr = rotationfilterArr.filter(result => result.hour === i);
+    let h = moment.duration(i, 'h');
+    let headTitle = firstOfWeek.clone().add(h).format("hh a")
+    calendarArr.push({id:''+i, headTitle,  data:tempArr});
+    }
+    return calendarArr;
+  }
+const finalData = calendarArrayCreator([{endTime: moment("2018-03-06"), 
+                  startTime: moment("2018-03-05"), taskName:"first"},
+                  { startTime: moment("2018-03-05T14:56:55Z"), 
+                  endTime: moment("2018-03-06T14:56:55Z"), taskName:"second"}], new Date(), 0);
 
 const CustomLayoutSpring = {
   duration: 400,
@@ -92,7 +210,7 @@ export default class App extends Component {
   }
   
   componentDidMount(){
-
+    // moment("").isBetween(moment("2018-03-03"), )
     Dimensions.addEventListener('change', this._handleChange);
 
   }
@@ -157,7 +275,7 @@ export default class App extends Component {
         }
       </View>
         <FlatList
-        data={bodyArr}
+        data={finalData}
         extraData={this.state}
         keyExtractor={this._keyExtractor}
         renderItem={this.renderItem}
